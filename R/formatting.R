f447d443ae58f912a534de13744ccab493c85e4b
const-ae <artjom31415@googlemail.com>
2024-11-14 11:20:06 +0000

    Move formatting functions to separate file

---
+++ R/formatting.R
@@ -0,0 +1,34 @@
+
+
+
+
+
+#' @export
+format.treelabel <- function(x, ...){
+  names <- tl_name(x)
+  scores <- tl_get_score(x, names)
+  paste0(names, "(", sprintf("%.2f", scores), ")")
+}
+
+
+#' @importFrom vctrs obj_print_footer
+#' @export
+obj_print_footer.treelabel <- function(x, ...){
+  vertex_names <- .tree_vertex_names(attr(x, "tree"))
+  cat("# Tree: ", toString(vertex_names, width = 40), "\n", sep = "")
+}
+
+#' @importFrom vctrs vec_ptype_abbr
+#' @export
+vec_ptype_abbr.treelabel <- function(x, ...) {
+  "tl"
+}
+
+
+.tree_vertex_names <- function(tree){
+  igraph::V(tree) |> igraph::as_ids()
+}
+
+.label_names <- function(x){
+  .tree_vertex_names(attr(x, "tree"))
+}
--- R/treelabel.R
+++ R/treelabel.R
@@ -115,38 +115,6 @@ new_treelabel <- function(data, tree){
 
 
 
-
-
-#' @export
-format.treelabel <- function(x, ...){
-  names <- tl_name(x)
-  scores <- tl_get_score(x, names)
-  paste0(names, "(", sprintf("%.2f", scores), ")")
-}
-
-
-#' @importFrom vctrs obj_print_footer
-#' @export
-obj_print_footer.treelabel <- function(x, ...){
-  vertex_names <- .tree_vertex_names(attr(x, "tree"))
-  cat("# Tree: ", toString(vertex_names, width = 40), "\n", sep = "")
-}
-
-#' @importFrom vctrs vec_ptype_abbr
-#' @export
-vec_ptype_abbr.treelabel <- function(x, ...) {
-  "tl"
-}
-
-
-.tree_vertex_names <- function(tree){
-  igraph::V(tree) |> igraph::as_ids()
-}
-
-.label_names <- function(x){
-  .tree_vertex_names(attr(x, "tree"))
-}
-
 .clamp <- function(x, min = 0, max = 1){
   pmin(x, max) |> pmax(min)
 }
