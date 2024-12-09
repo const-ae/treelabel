ea175d242caca738f9b453744c6768d11997ea1e
const-ae <artjom31415@googlemail.com>
2024-12-09 15:28:37 +0000

    Remove tl_is

--- R/tl_eval.R
+++ R/tl_eval.R
@@ -7,8 +7,19 @@
 #'   score is higher than a threshold
 #' @param ... additional arguments passed on to `rlang::eval_tidy`
 #'
+#' **Note**: Do not perform any stateful calculation in `expr` as it
+#'   is evaluated multiple times
+#'
 #' @export
 tl_eval <- function(x, expr, ...){
+  stopifnot(is_treelabel(x))
+  # A <- .eval_impl(x, {{expr}}, ...)
+  B <- .eval_impl(tl_atmost(x), {{expr}}, ...)
+  C <- .eval_impl(tl_atleast(x), {{expr}}, ...)
+  ifelse(B == C, B, NA)
+}
+
+.eval_impl <- function(x, expr, ...){
   expr <- rlang::enquo(expr)
   data <- tl_score_matrix(x)
   data_tib <- tibble::as_tibble(data)
@@ -37,17 +48,18 @@ tl_get <- function(x, name){
   data[col * nrow(data) + seq_len(nrow(data)) - 1 + 1]
 }
 
-#'
-#' @rdname tl_eval
-#' @export
-tl_is <- function(x, expr, ...){
-  A <- tl_eval(x, {{expr}}, ...)
-  stopifnot(rlang::is_logical(A))
-  B <- tl_eval(tl_atmost(x), {{expr}}, ...)
-  stopifnot(rlang::is_logical(B))
-  # This takes A if A is not NA.
-  # If is.na(A) the result is FALSE if B is FALSE,
-  # otherwise it is NA.
-  A %|% (A & B)
-}
+# #'
+# #' @rdname tl_eval
+# #' @export
+# tl_is <- function(x, expr, ...){
+#   A <- tl_eval(x, {{expr}}, ...)
+#   stopifnot(rlang::is_logical(A))
+#   B <- tl_eval(tl_atmost(x), {{expr}}, ...)
+#   C <- tl_eval(tl_atleast(x), {{expr}}, ...)
+#   stopifnot(rlang::is_logical(B))
+#   # This takes A if A is not NA.
+#   # If is.na(A) the result is FALSE if B is FALSE,
+#   # otherwise it is NA.
+#   A %|% ifelse(A == B, A, NA)
+# }
 
