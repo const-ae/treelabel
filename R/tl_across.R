60d0b62bb79a0cf4069c528922c986cd65247d8a
const-ae <artjom31415@googlemail.com>
2025-01-14 16:42:19 +0000

    Simplify `tl_across` and derivatives

--- R/tl_across.R
+++ R/tl_across.R
@@ -4,73 +4,68 @@
 #' @param .cols tidyselect expression of the column which contain the treelabel
 #'   columns
 #' @param expr an unquoted expression evaluated with `tl_eval` for each column.
+#' @param ... addtional parameters passed on to `across`
 #' @param na.rm boolean that decides if missing values are ignored.
-#' @param mode the evaluation subtly differs if the results is numeric or logical
 #'
 #' @seealso [dplyr::across()]
 #'
 #' @export
-tl_across <- function(.cols, expr, mode = c("logical", "numeric")){
-  mode <- rlang::arg_match(mode)
-  eval_fnc <- switch (mode,
-    logical = tl_is,
-    numeric = tl_eval
-  )
+tl_across <- function(.cols, expr, ...){
   dplyr::across({{.cols}}, \(x){
     stopifnot(is_treelabel(x))
-    eval_fnc(x, {{expr}})
-  })
+    tl_eval(x, {{expr}})
+  }, ...)
 }
 
 #' @export
 #' @rdname tl_across
-tl_if_any <- function(.cols, expr){
+tl_if_any <- function(.cols, expr, ...){
   dplyr::if_any({{.cols}}, \(x){
     stopifnot(is_treelabel(x))
     tl_is(x, {{expr}})
-  })
+  }, ...)
 }
 
 #' @export
 #' @rdname tl_across
-tl_if_all <- function(.cols, expr){
+tl_if_all <- function(.cols, expr, ...){
   dplyr::if_all({{.cols}}, \(x){
     stopifnot(is_treelabel(x))
     tl_is(x, {{expr}})
-  })
+  }, ...)
 }
 
 #' @export
 #' @rdname tl_across
-tl_sum_across <- function(.cols, expr, na.rm = TRUE){
-  mat <- as.matrix(tl_across(.cols = {{.cols}}, {{expr}}, mode = "numeric"))
+tl_sum_across <- function(.cols, expr, ..., na.rm = TRUE){
+  mat <- as.matrix(tl_across(.cols = {{.cols}}, {{expr}}, ...))
   rowSums(mat, na.rm = na.rm)
 }
 
 #' @export
 #' @rdname tl_across
-tl_mean_across <- function(.cols, expr, na.rm = TRUE){
-  mat <- as.matrix(tl_across({{.cols}}, {{expr}}, mode = "numeric"))
+tl_mean_across <- function(.cols, expr, ..., na.rm = TRUE){
+  mat <- as.matrix(tl_across({{.cols}}, {{expr}}, ...))
   rowMeans(mat, na.rm = na.rm)
 }
 
-#' @export
-#' @rdname tl_across
-tl_count_across <- function(.cols, expr, na.rm = TRUE){
-  mat <- as.matrix(tl_across({{.cols}}, {{expr}}, mode = "logical"))
-  rowSums(mat, na.rm = na.rm)
-}
+#' #' @export
+#' #' @rdname tl_across
+#' tl_count_across <- function(.cols, expr, ..., na.rm = TRUE){
+#'   mat <- as.matrix(tl_across({{.cols}}, {{expr}}, ...))
+#'   rowSums(mat, na.rm = na.rm)
+#' }
+#'
+#' #' @export
+#' #' @rdname tl_across
+#' tl_fraction_across <- function(.cols, expr, ..., na.rm = TRUE){
+#'   mat <- as.matrix(tl_across({{.cols}}, {{expr}}, ...))
+#'   rowMeans(mat, na.rm = na.rm)
+#' }
 
 #' @export
 #' @rdname tl_across
-tl_fraction_across <- function(.cols, expr, na.rm = TRUE){
-  mat <- as.matrix(tl_across({{.cols}}, {{expr}}, mode = "logical"))
-  rowMeans(mat, na.rm = na.rm)
-}
-
-#' @export
-#' @rdname tl_across
-tl_countNAs_across <- function(.cols, expr, na.rm = TRUE){
-  mat <- as.matrix(tl_across({{.cols}}, {{expr}}))
+tl_countNAs_across <- function(.cols, expr, ..., na.rm = TRUE){
+  mat <- as.matrix(tl_across({{.cols}}, {{expr}}, ...))
   matrixStats::rowCounts(mat, value = NA)
 }
